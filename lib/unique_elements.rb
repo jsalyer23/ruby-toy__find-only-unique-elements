e51f0d42adf465f233ab549788cb10a6f7101a7c
Josh Salyer <joshsalyer23@gmail.com>
2016-06-07 00:35:09 +0000

    Uniqueness Test

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,15 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+    #select each element of the array and compare each one to the rest of the array
+    arr.each_index do |idx|
+    #compare each element within the array
+        if arr[idx] == arr[idx+1]
+            arr.delete_at(idx)
+            #delete the first matching element
+            arr.delete_at(idx+1)
+            #delete the element that was matched
+        end
+    #return/print out the remaining elements within the array
+    end
 end
